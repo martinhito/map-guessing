7ba2c5ae6d8d7857a4cc7b549c4c79ec3cdc075d
martinhito <martin@hito.us>
2025-12-26 23:48:10 -0500

    add vercel URL to cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -29,6 +29,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",  # Next.js dev server
         "http://127.0.0.1:3000",
+        "https://map-guessing.vercel.app",  # Production
     ],
     allow_credentials=True,
     allow_methods=["*"],
